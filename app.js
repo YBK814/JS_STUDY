ae8fff905bdcdb09a3a42b77809ca27c703a2607
YBK814 <romandawn0814@gmail.com>
2023-06-24 13:22:50 +0900

    2.4

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const a = 5;
-const b = 2;
-let myName ="yebhin"
-// 작성자의 의도를 알 수 있는 const(고정값)와 let(변형값)의 차이
-// 대부분 const 때때로 let 하지만 var 쓰지 말 것
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "Kim Yebhin"
-
-console.log("Your new name is " + myName);
+const amIFat = null;
+// 의도적으로 비어있는 값
+let something;
+// 정의되지 않은 값
+console.log(something);
+console.log(amIFat);
