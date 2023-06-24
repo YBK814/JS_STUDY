b4970ffe4955d93cfb7357e5ec6ce2303dee1aa4
YBK814 <romandawn0814@gmail.com>
2023-06-24 13:14:31 +0900

    2.3강의

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const a = 5;
 const b = 2;
-
-const myName ="yebhin"
+let myName ="yebhin"
+// 작성자의 의도를 알 수 있는 const(고정값)와 let(변형값)의 차이
+// 대부분 const 때때로 let 하지만 var 쓰지 말 것
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello" + myName);
-console.log("hello" + myName);
+console.log("hello " + myName);
+
+myName = "Kim Yebhin"
+
+console.log("Your new name is " + myName);
